18acc5023b44a66f59f6d96ed10e42e537b9b4b3
Guillermo <guilletgn@msn.com>
2013-12-27 12:30:23 +0100

    edit 5

--- ProGit/src/absis/Clase5.java
+++ ProGit/src/absis/Clase5.java
@@ -5,5 +5,6 @@ public class Clase5 {
 	//Línea1 (En la versión 1.2 se creó esta clase y una rama para desarrollarla pero 
 	// en la versión 1.5 todavía no se ha tocado esta rama. Después de este commit, haremos un merge
 	// al master. Intuyo que en el history quedará una línea larga tal y como b6.)
-
+	
+	//Línea2 de b10
 }
